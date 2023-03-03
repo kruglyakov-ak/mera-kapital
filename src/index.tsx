f9206aff4cead75c99a11cb18f8cec32f05fc81a
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2023-03-03 22:43:30 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { setupStore } from "./store/store";
-import App from "./app";
+import App from "./App";
 import "./styles/global.scss";
 
 const container = document.getElementById("root")!;
